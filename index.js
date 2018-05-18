9c8a3dc82f875b07447f76fd1abba3f6e25cd37d
PisuChan <33348933+Pisuton@users.noreply.github.com>
2018-05-18 18:15:23 +0900

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = 'c/';
 
+client.on('ready', () => {
+	console.log('ログインしました。');
+	client.user.setActivity(`Type ${prefix}help`);
+});
